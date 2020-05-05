9ee663763bd95c7782ce0ed6a2f7490421645198
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-05-05 20:37:37 +0200

    tinker with types

--- src/Statistics/Mcmc/Types.hs
+++ src/Statistics/Mcmc/Types.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE RankNTypes #-}
+
 {- |
 Module      :  Statistics.Mcmc.Types
 Description :  What is an MCMC?
@@ -10,41 +13,100 @@ Portability :  portable
 
 Creation date: Tue May  5 18:01:15 2020.
 
+TODO: Think about how to save and restore an MCMC run. It is easy to save and
+restore the current state and likelihood (or the trace), but it seems impossible
+to store all the moves and so on.
+
+TODO: Maybe rename Chain to Trace; the Chain seems to be the list of States, and
+the Trace includes the log-likelihoods and so on.
+
 -}
 
 module Statistics.Mcmc.Types
-  ( State (..)
+  ( -- * Static types
+    --
+    -- Types related to the status of an MCMC.
+    State (..)
   , Mcmc (..)
   , Link (..)
   , Chain (..)
+    -- * Dynamic types
+    --
+    -- Types related to moving between states.
+  , Move (..)
+  , Cycle (..)
   ) where
 
+import Control.Monad.Primitive
 import Numeric.Log
+import System.Random.MWC
 
--- | A state in the parameter space @a@.
+-- | A 'State' in the state space @a@.
 newtype State a = State a
-  deriving (Show, Read)
+  deriving (Eq, Ord, Show, Read, Functor)
 
+-- | An MCMC is defined by the current 'State' in the state space @a@, a
+-- log-likelihood function, and a set of 'Move's forming a 'Cycle'. The wrapper
+-- @m@ provides a source of randomness.
+--
+-- We could think of adding a set of moves, or something equivalent, but we
+-- don't do that at the moment :).
 data Mcmc a = Mcmc
   {
-    -- ^ The present state.
-    state :: State a
+    -- ^ The current state.
+    mcmcState :: State a
     -- ^ The log-likelihood function.
-  , llhf  :: State a -> Log Double
-    -- We could think of adding a set of moves, or something equivalent, but we
-    -- don't do that at the moment :).
+  , mcmcLlhf  :: State a -> Log Double
   }
 
--- | A link in the chain.
+-- | A 'Link' of the 'Chain'. For simplicity, we associate each passed 'State'
+-- with the corresponding log-likelihood. A list of 'Link's is what makes a
+-- 'Chain'.
 data Link a = Link
   {
-    -- ^ The present state.
-    lnState :: State a
-    -- ^ The present log-likelihood.
-  , lnLlh   :: Log Double
+    -- ^ The current state.
+    lnState   :: State a
+    -- ^ The current log-likelihood.
+  , lnLlh     :: Log Double
   }
-  deriving (Show, Read)
+  deriving (Eq, Ord, Show, Read)
 
--- | A chain passes through a list of states.
+-- | A 'Chain' passes through a list of 'State's with associated log-likelihoods
+-- called 'Link's.
 newtype Chain a = Chain [Link a]
   deriving (Show, Read)
+
+instance Semigroup (Chain a) where
+  (Chain l) <> (Chain r) = Chain (l <> r)
+
+instance Monoid (Chain a) where
+  mempty = Chain []
+
+-- XXX: It may be advantageous to include these types into the Metropolis module.
+
+-- | A 'Move' is an instruction about how the MCMC will traverse the 'State'
+-- space. Essentially, it is a probability distribution conditioned on the
+-- current 'State'.
+--
+-- We need to be able to sample a new state.
+--
+-- We need to know the probability of jumping forth, but also the probability of
+-- jumping back. They are needed to calculate the Metropolis-Hastings ratio.
+data Move m a = Move
+  {
+    -- ^ Instruction about sampling a new state.
+    mvSample :: PrimMonad m => Gen (PrimState m) -> State a -> m (State a)
+    -- ^ The log-likelihood of going from one state to another.
+  , mvLlh :: State a -> State a -> Log Double
+  }
+
+-- | A collection of 'Move's form a 'Cycle'. The 'State' of the 'Chain' will be
+-- logged after each 'Cycle'.
+newtype Cycle m a = Cycle [Move m a]
+
+instance Semigroup (Cycle m a) where
+  (Cycle l) <> (Cycle r) = Cycle (l <> r)
+
+instance Monoid (Cycle m a) where
+  mempty = Cycle []
+
