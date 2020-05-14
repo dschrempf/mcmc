03af8b98c2ac9a4a66264a26dae7bb832bbebbbe
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-05-14 21:48:45 +0200

    continuous and discrete generic moves

--- src/Statistics/Mcmc/Moves/Generic.hs
+++ src/Statistics/Mcmc/Moves/Generic.hs
@@ -15,7 +15,8 @@ Creation date: Thu May 14 20:26:27 2020.
 -}
 
 module Statistics.Mcmc.Moves.Generic
-  ( moveGeneric
+  ( moveGenericContinuous
+  , moveGenericDiscrete
   ) where
 
 import Lens.Micro
@@ -25,7 +26,7 @@ import System.Random.MWC
 
 import Statistics.Mcmc.Types
 
-jump
+jumpCont
   :: (ContDistr d, ContGen d)
   => Lens' a Double
   -> d
@@ -33,28 +34,62 @@ jump
   -> a
   -> GenIO
   -> IO a
-jump l d f x g = do
+jumpCont l d f x g = do
   dx <- genContVar d g
   return $ set l ((x^.l) `f` dx) x
-{-# INLINEABLE jump #-}
+{-# INLINEABLE jumpCont #-}
 
 -- XXX: Technically, only a Getter is needed here.
-logDens
+logDensCont
   :: (ContDistr d, ContGen d)
   => Lens' a Double
   -> d
   -> (Double -> Double -> Double)
   -> a -> a -> Log Double
-logDens l d fInv x y = Exp $ logDensity d ((y^.l) `fInv` (x^.l))
-{-# INLINEABLE logDens #-}
+logDensCont l d fInv x y = Exp $ logDensity d ((y^.l) `fInv` (x^.l))
+{-# INLINEABLE logDensCont #-}
 
--- | A symmetric move with normally distributed density.
-moveGeneric
+-- | Generic function to create moves for continuous parameters ('Double').
+moveGenericContinuous
   :: (ContDistr d, ContGen d)
-  => Lens' a Double     -- ^ Instruction about which parameter to change.
-  -> String             -- ^ Name.
-  -> d                  -- ^ Probability distribution.
+  => Lens' a Double               -- ^ Instruction about which parameter to change.
+  -> String                       -- ^ Name of the move.
+  -> d                            -- ^ Probability distribution.
   -> (Double -> Double -> Double) -- ^ Forward operator, e.g. (+), so that x + dx = y.
   -> (Double -> Double -> Double) -- ^ Inverse operator, e.g.,(-), so that y - dx = x.
   -> Move a
-moveGeneric l n d f fInv = Move n (jump l d f) (logDens l d fInv)
+moveGenericContinuous l n d f fInv = Move n (jumpCont l d f) (logDensCont l d fInv)
+
+jumpDiscrete
+  :: (DiscreteDistr d, DiscreteGen d)
+  => Lens' a Int
+  -> d
+  -> (Int -> Int -> Int)
+  -> a
+  -> GenIO
+  -> IO a
+jumpDiscrete l d f x g = do
+  dx <- genDiscreteVar d g
+  return $ set l ((x^.l) `f` dx) x
+{-# INLINEABLE jumpDiscrete #-}
+
+-- XXX: Technically, only a Getter is needed here.
+logDensDiscrete
+  :: (DiscreteDistr d, DiscreteGen d)
+  => Lens' a Int
+  -> d
+  -> (Int -> Int -> Int)
+  -> a -> a -> Log Double
+logDensDiscrete l d fInv x y = Exp $ logProbability d ((y^.l) `fInv` (x^.l))
+{-# INLINEABLE logDensDiscrete #-}
+
+-- | Generic function to create moves for discrete parameters ('Int').
+moveGenericDiscrete
+  :: (DiscreteDistr d, DiscreteGen d)
+  => Lens' a Int         -- ^ Instruction about which parameter to change.
+  -> String              -- ^ Name of the move.
+  -> d                   -- ^ Probability distribution.
+  -> (Int -> Int -> Int) -- ^ Forward operator, e.g. (+), so that x + dx = y.
+  -> (Int -> Int -> Int) -- ^ Inverse operator, e.g.,(-), so that y - dx = x.
+  -> Move a
+moveGenericDiscrete l n d f fInv = Move n (jumpDiscrete l d f) (logDensDiscrete l d fInv)
--- src/Statistics/Mcmc/Moves/Slide.hs
+++ src/Statistics/Mcmc/Moves/Slide.hs
@@ -17,70 +17,24 @@ Creation date: Wed May  6 10:59:13 2020.
 module Statistics.Mcmc.Moves.Slide
   ( slide
   , slideDouble
-  , slideGeneric
   ) where
 
 import Lens.Micro
-import Numeric.Log
-import Statistics.Distribution
 import Statistics.Distribution.Normal
-import System.Random.MWC
 
 import Statistics.Mcmc.Types
 import Statistics.Mcmc.Moves.Generic
 
-delta :: Lens' a Double -> NormalDistribution -> a -> GenIO -> IO a
-delta l d x g = do
-  dx <- genContVar d g
-  return $ set l (x^.l + dx) x
-{-# INLINEABLE delta #-}
-
--- XXX: Technically, only a Getter is needed here.
-logDens :: Lens' a Double -> NormalDistribution -> a -> a -> Log Double
-logDens l d x y = Exp $ logDensity d (y^.l - x^.l)
-{-# INLINEABLE logDens #-}
-
--- | A symmetric move with normally distributed density.
+-- | Additive move with normally distributed density.
 slide
   :: Lens' a Double     -- ^ Instruction about which parameter to change.
   -> String             -- ^ Name.
   -> Double             -- ^ Mean.
   -> Double             -- ^ Standard deviation.
   -> Move a
-slide l n m s = Move n (delta l d) (logDens l d)
-  where d = normalDistr m s
+slide l n m s = moveGenericContinuous l n (normalDistr m s) (+) (-)
 
-slideGeneric
-  :: Lens' a Double     -- ^ Instruction about which parameter to change.
-  -> String             -- ^ Name.
-  -> Double             -- ^ Mean.
-  -> Double             -- ^ Standard deviation.
-  -> Move a
-slideGeneric l n m s = moveGeneric l n (normalDistr m s) (+) (-)
-
--- | A symmetric move with normally distributed density; specialized to a one
+-- | Additive move with normally distributed density; specialized to a one
 -- dimensional state space of type 'Double'; see 'moveNormal'.
 slideDouble :: String -> Double -> Double -> Move Double
 slideDouble = slide id
-
--- -- It turns out that direct implementation is not faster (or only marginally
--- -- faster); tested with criterion for 50000 jumps.
-
--- deltaDouble :: NormalDistribution -> Double -> Double -> GenIO -> IO Double
--- deltaDouble d m x g = do
---   dx <- genContVar d g
---   return $ (x + m + dx)
--- {-# INLINEABLE deltaDouble #-}
-
--- logDensDouble :: NormalDistribution -> Double -> Double -> Log Double
--- logDensDouble d x y = Exp $ logDensity d (y - x)
--- {-# INLINEABLE logDensDouble #-}
-
--- -- | A symmetric move with normally distributed density.
--- moveNormalDoubleFast
---   :: String             -- ^ Name.
---   -> Double             -- ^ Mean.
---   -> Double             -- ^ Standard deviation.
---   -> Move Double
--- moveNormalDoubleFast n m s = Move n (deltaDouble d m) (logDensDouble d)
---   where d = normalDistr 0 s
