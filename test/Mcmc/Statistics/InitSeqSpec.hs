95c188cc1a1366ce89a0c443a4c63a7fb08cb860
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-06-06 16:47:55 +0200

    one new test

--- test/Mcmc/Statistics/InitSeqSpec.hs
+++ test/Mcmc/Statistics/InitSeqSpec.hs
@@ -1,6 +1,6 @@
 {- |
    Module      :  Mcmc.Statistics.InitSeqSpec
-   Description :  Unit tests for Mcmc.Statistics.InitSeqSpec
+   Description :  Unit tests for Mcmc.Statistics.InitSeq
    Copyright   :  (c) Dominik Schrempf, 2020
    License     :  GPL-3.0-or-later
 
@@ -40,11 +40,25 @@ xsWeird = V.fromList [-2001, 7293, 9128]
 ysWeird :: Vector Double
 ysWeird = V.fromList [88, 22, 920938]
 
+os2 :: Vector Double
+os2 = V.fromList [2.39604875, 0.59133253, 0.49601200, 0.77828088, 0.36678584,
+                  0.81875424, 0.59677332, 2.32285757, 0.13094589, 0.45258170,
+                  0.10324180, 2.56827353, 0.50313726, 0.31220387, 1.33664320,
+                  1.24568427, 0.80642147, 1.69867356, 0.03597119, 0.82510016,
+                  0.95890956, 2.90166793, 0.67492229, 0.21564363]
+
+xs2 :: Vector Int
+xs2 = V.fromList [0,1,2,4,8,10,18,23]
+
+ys2 :: Vector Double
+ys2 = V.fromList [2.39604875,0.59133253,0.496012,0.36678584,0.13094589,0.1032418,3.597119e-2,0.21564363]
+
 spec :: Spec
 spec = do
   describe "gcm" $ do
-    it "returns the greatest convex minorant" $
+    it "returns the greatest convex minorant" $ do
       gcm os `shouldBe` (xs, ys)
+      gcm os2 `shouldBe` (xs2, ys2)
     it "shouldn't fail on a singleton vector" $
       gcm (V.singleton 1.0) `shouldBe` (V.singleton 0, V.singleton 1.0)
     it "shouldn't fail on an empty vector" $
