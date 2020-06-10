133a1fa6a5516e7c5dcf5ea6eebadb23e2778984
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-06-11 01:04:02 +0200

    basic implementation works!

--- app/ApproximatePhylogeneticLikelihood/Main.hs
+++ app/ApproximatePhylogeneticLikelihood/Main.hs
@@ -12,19 +12,24 @@ Creation date: Wed Jun 10 22:07:11 2020.
 
 -}
 
-module ApproximatePhylogeneticLikelihood
-  ()
+module Main
+  ( main
+  )
 where
 
 import           Algebra.Graph.Label
-import           Algebra.Graph.Labelled
+-- import           Algebra.Graph.Labelled
+import           Algebra.Graph.Labelled.AdjacencyMap
+import           Control.Monad
 import           Data.Maybe
+import qualified Data.Text.Lazy             as T
 import           Lens.Micro
 import           Numeric.Log
 import           Statistics.Distribution hiding ( Mean )
 import           Statistics.Distribution.Normal
+import           System.Random.MWC
 
-import           Mcmc.Move
+import           Mcmc
 import           Mcmc.Move.Generic
 
 type Length = Double
@@ -35,13 +40,13 @@ type StdDev = Double
 type Node = Int
 
 -- Tree with posterior means as branches.
-type MTree = Graph (Distance Mean) Node
+type MTree = AdjacencyMap (Distance Mean) Node
 
 -- Tree with posterior standard deviations as branches.
-type VTree = Graph (Distance StdDev) Node
+type VTree = AdjacencyMap (Distance StdDev) Node
 
 -- The tree with current branch lengths, which is also the state of the chain.
-type LTree = Graph (Distance Length) Node
+type LTree = AdjacencyMap (Distance Length) Node
 type I = LTree
 
 -- For now, use a completely uninformative prior.
@@ -62,27 +67,27 @@ llh mt vt lt = product $ zipWith3 llhBranch ms vs ls
   vs = map (fromD . (^. _1)) $ edgeList vt
   ls = map (fromD . (^. _1)) $ edgeList lt
 
-setter :: (Monoid e, Eq e, Ord a) => a -> a -> e -> Graph e a -> Graph e a
+setter :: (Monoid e, Eq e, Ord a) => a -> a -> e -> AdjacencyMap e a -> AdjacencyMap e a
 setter x y e = replaceEdge e x y
 
-getter :: (Monoid e, Eq a) => a -> a -> Graph e a -> e
+getter :: (Monoid e, Eq a, Ord a) => a -> a -> AdjacencyMap e a -> e
 getter = edgeLabel
 
-allEdges :: (Eq e, Monoid e, Ord a) => Graph e a -> [(a, a)]
+allEdges :: (Eq e, Monoid e, Ord a) => AdjacencyMap e a -> [(a, a)]
 allEdges = map (\(_, x, y) -> (x, y)) . edgeList
 
-slide :: Node -> Node -> Move I
-slide x y = Move n 1 slideSimple Nothing
+slideBr :: Node -> Node -> Move I
+slideBr x y = Move n 1 slideSimple Nothing
  where
   n           = show (x, y)
-  slideSimple = moveGenericContinuous (setter x y . distance . unsafeFinite)
+  slideSimple = moveGenericContinuous (setter x y . toD)
                                       (fromD . getter x y)
-                                      (normalDistr 0 1.0)
+                                      (normalDistr 0 2.0)
                                       (+)
                                       (-)
 
 moveCycle :: Cycle I
-moveCycle = fromList $ map (uncurry slide) (allEdges lTree)
+moveCycle = fromList $ map (uncurry slideBr) (allEdges lTree)
 
 toD :: a -> Distance a
 toD = distance . unsafeFinite
@@ -99,10 +104,30 @@ mTree = (0 -< toD 5.0 >- 1) + (0 -< toD 10.0 >- 2)
 vTree :: MTree
 vTree = (0 -< toD 2.0 >- 1) + (0 -< toD 2.0 >- 2)
 
+mons :: [MonitorParameter I]
+mons = [ monitorRealFloat (n x y) (l x y) | (x, y) <- allEdges lTree ]
+         where n x y = T.pack $ show (x, y)
+               g x y = fromD . getter x y
+               s x y gr e = replaceEdge (toD e) x y gr
+               l x y = lens (g x y) (s x y)
+
+monStd :: MonitorStdOut I
+monStd = monitorStdOut mons 10
+
+monFile :: MonitorFile I
+monFile = monitorFile "ApproximatePhylogeneticLikelihood.log" mons 10
+
+mon :: Monitor I
+mon = Monitor monStd [monFile]
+
+nBurn :: Maybe Int
+nBurn = Just 2000
+
+nIter :: Int
+nIter = 10000
+
 main :: IO ()
 main = do
   g           <- create
-  mu_observed <- arrowMean g
-  -- putStrLn $ "True parameter: " <> show mu_observed
-  let s = status 0.01 prior (likelihood mu_observed) moveCycle mon g
-  void $ mh nBurn nAutoTune nIter s
+  let s = status lTree prior (llh mTree vTree) moveCycle mon g
+  void $ mh nBurn Nothing nIter s
