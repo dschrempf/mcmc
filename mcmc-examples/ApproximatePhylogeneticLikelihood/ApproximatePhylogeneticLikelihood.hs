20c61ea2bbb54fc52c15786ec69c72f1e82810cb
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-06-24 14:28:39 +0200

    more comments

--- mcmc-examples/ApproximatePhylogeneticLikelihood/ApproximatePhylogeneticLikelihood.hs
+++ mcmc-examples/ApproximatePhylogeneticLikelihood/ApproximatePhylogeneticLikelihood.hs
@@ -32,19 +32,19 @@ import Statistics.Distribution.Normal
 import System.Environment
 import System.Random.MWC
 
--- | Node (vertex) label.
+-- Node (vertex) label.
 type Node = Int
 
--- | Branch length.
+-- Branch length.
 type Length = Double
 
--- | Mean.
+-- Mean.
 type Mean = Double
 
--- | Standard deviation.
+-- Standard deviation.
 type StdDev = Double
 
--- | The state space is a tree. We apply the branch length to the 'Distance'
+-- The state space is a tree. We apply the branch length to the 'Distance'
 -- data constructor. In this way, we tell the graph algorithms that
 --
 -- - branches lengths have to be added together when calculating the length of a
@@ -60,6 +60,10 @@ type StdDev = Double
 -- lengths, or the means and standard deviations calculated previously.
 type Tree a = AdjacencyMap (Distance Length) Node
 
+-- We want to save and restore the Markov chain. This is done by converting
+-- trees to and from JSON objects. It is unfortunate, that we have to tell
+-- Haskell how to convert 'Distance' and 'Tree' objects to and from JSON.
+
 instance ToJSON (Distance Double) where
   toJSON = toJSON . fromD
   toEncoding = toEncoding . fromD
@@ -67,119 +71,117 @@ instance ToJSON (Distance Double) where
 instance FromJSON (Distance Double) where
   parseJSON d = toD <$> (parseJSON d :: Parser Double)
 
--- | We want to save and restore the Markov chain. This is done by converting
--- trees to adn from JSON objects.
 instance ToJSON (Tree a)
 
 instance FromJSON (Tree a)
 
--- | Branch accessor functions (lenses).
+-- Branch accessor functions (lenses).
 getLens :: Node -> Node -> Lens' (Tree a) Double
 getLens x y = lens (g x y) (s x y)
   where
     g v w = fromD . edgeLabel v w
     s n m gr e = replaceEdge (toD e) n m gr
 
--- | Uninformative prior.
+-- Uninformative prior.
 pr :: a -> Log Double
 pr = const 1
 
--- | Branch likelihood, a normal distribution with given mean and standard deviation.
+-- Branch likelihood, a normal distribution with given mean and standard deviation.
 lhBranch :: Mean -> StdDev -> Length -> Log Double
 lhBranch m s l
   | l <= 0 = pzero
   | otherwise = Exp $ logDensity (normalDistr m s) l
 
--- | Likelihood of the tree for two given trees containing the branch length
+-- Likelihood of the tree for two given trees containing the branch length
 -- means and standard deviations.
 lh :: Tree Mean -> Tree StdDev -> Tree Length -> Log Double
 lh mt st lt = product $ zipWith3 lhBranch (getEdgeLabels mt) (getEdgeLabels st) (getEdgeLabels lt)
 
--- | Get a list of edge labels of a tree.
+-- Get a list of edge labels of a tree.
 getEdgeLabels :: Tree a -> [Double]
 getEdgeLabels = map (fromD . (^. _1)) . edgeList
 
--- | Get a list of edges without labels of a tree.
+-- Get a list of edges without labels of a tree.
 getEdges :: Tree a -> [(Node, Node)]
 getEdges = map (\(_, x, y) -> (x, y)) . edgeList
 
--- | Sliding move changing the length of a branch.
+-- Sliding move changing the length of a branch.
 slideBranch :: Node -> Node -> Move (Tree Length)
 slideBranch x y = slideSymmetric n 1 (getLens x y) 1.0 True
   where
     n = "Slide edge " <> show (x, y)
 
--- | Collect all sliding moves into a cycle.
+-- Collect all sliding moves into a cycle.
 moveCycle :: Cycle (Tree Length)
 moveCycle = fromList $ map (uncurry slideBranch) (getEdges startingTree)
 
--- | Convert a number to a distance. Assume that the given number is positive
+-- Convert a number to a distance. Assume that the given number is positive
 -- and finite.
 toD :: Num a => a -> Distance a
 toD = distance . unsafeFinite
 
--- | Extract the number from a distance. Assume that the distance is finite.
+-- Extract the number from a distance. Assume that the distance is finite.
 fromD :: Distance a -> a
 fromD = fromJust . getFinite . getDistance
 
--- | Starting tree; let's start far away from the truth.
+-- Starting tree; let's start far away from the truth.
 startingTree :: Tree Length
 startingTree = (0 -< toD 1.0 >- 1) + (0 -< toD 2.0 >- 2)
 
--- | Tree storing the true means.
+-- Tree storing the true means.
 meanTree :: Tree Mean
 meanTree = (0 -< toD 5.0 >- 1) + (0 -< toD 10.0 >- 2)
 
--- | Tree storing the true standard deviations.
+-- Tree storing the true standard deviations.
 stdDevTree :: Tree StdDev
 stdDevTree = (0 -< toD 2.0 >- 1) + (0 -< toD 2.0 >- 2)
 
--- | Branch length monitors.
+-- Branch length monitors.
 branchMons :: [MonitorParameter (Tree Length)]
 branchMons = [monitorRealFloat (n x y) (getLens x y) | (x, y) <- getEdges startingTree]
   where
     n x y = show (x, y)
 
--- | Monitor branch lengths to standard output.
+-- Monitor branch lengths to standard output.
 monStdOut :: MonitorStdOut (Tree Length)
 monStdOut = monitorStdOut branchMons 100
 
--- | Monitor branch lengths to a file.
+-- Monitor branch lengths to a file.
 monFile :: MonitorFile (Tree Length)
 monFile = monitorFile "Branches" branchMons 10
 
--- | Monitor batch means of branch lengths.
+-- Monitor batch means of branch lengths.
 branchBatchMons :: [MonitorParameterBatch (Tree Length)]
 branchBatchMons =
   [monitorBatchMeanRealFloat (n x y) (getLens x y) | (x, y) <- getEdges startingTree]
   where
     n x y = "Mean " <> show (x, y)
 
--- | Monitor batch means of branch lengths to a file.
+-- Monitor batch means of branch lengths to a file.
 monBatch :: MonitorBatch (Tree Length)
 monBatch = monitorBatch "Branches" branchBatchMons 200
 
--- | Combine the monitors.
+-- Combine the monitors.
 mon :: Monitor (Tree Length)
 mon = Monitor monStdOut [monFile] [monBatch]
 
--- | Number of burn in iterations.
+-- Number of burn in iterations.
 nBurnIn :: Maybe Int
 nBurnIn = Just 4000
 
--- | Auto tuning period.
+-- Auto tuning period.
 nAutoTune :: Maybe Int
 nAutoTune = Just 200
 
--- | Number of Metropolis-Hasting iterations after burn in.
+-- Number of Metropolis-Hasting iterations after burn in.
 nIterations :: Int
 nIterations = 20000
 
--- | Name of the chain; used as prefix of the output file names.
+-- Name of the chain; used as prefix of the output file names.
 nm :: String
 nm = "ApproximatePhylogeneticLikelihood"
 
--- | The main program.
+-- The main program.
 main :: IO ()
 main = do
   -- Get the command line arguments and check if a new chain is to be started,
