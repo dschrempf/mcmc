9ea6336b1221e92e892c2ed5b15cec696166b19d
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-10-20 13:52:45 +0200

    documentation

--- mcmc/src/Mcmc/Proposal/Simplex.hs
+++ mcmc/src/Mcmc/Proposal/Simplex.hs
@@ -10,7 +10,7 @@
 --
 -- Creation date: Mon Oct 19 15:32:31 2020.
 module Mcmc.Proposal.Simplex
-  ( -- * Simplices
+  ( -- * Elements of simplices
     Simplex (toVector),
     simplexUniform,
     simplexFromVector,
@@ -26,12 +26,14 @@ import qualified Data.Vector.Unboxed as V
 import Mcmc.Proposal
 import Statistics.Distribution.Dirichlet
 
--- | A vector of non-negative values summing to one.
+-- | An element of a simplex.
 --
--- The nomenclature is not very consistent, because a __D-Simplex__ is usually
--- considered to be the set containing all @D@-dimensional vectors with
+-- A vector of non-negative values summing to one.
+--
+-- The nomenclature is not very consistent, because a K-dimensional simplex is
+-- usually considered to be the set containing all @K@-dimensional vectors with
 -- non-negative elements that sum to 1.0. However, I couldn't come up with a
--- better name. Maybe @ElementOfSimplex@, but that was too long.
+-- better name. Maybe @SimplexElement@, but that was too long.
 newtype Simplex = Simplex {toVector :: V.Vector Double}
   deriving (Eq, Show)
 
@@ -49,7 +51,7 @@ isNormalized v
 isNegative :: V.Vector Double -> Bool
 isNegative = V.any (< 0)
 
--- | Create a simplex from a vector.
+-- | Ensure that the value vector is an element of a simplex.
 --
 -- Return 'Left' if:
 -- - The value vector is empty.
@@ -62,24 +64,30 @@ simplexFromVector v
   | not (isNormalized v) = Left "simplexFromVector: Vector is not normalized."
   | otherwise = Right $ Simplex v
 
--- | Set all values to \(1/n\).
+-- | Create the uniform element of the K-dimensional simplex.
+--
+-- Set all values to \(1/D\).
 simplexUniform :: Int -> Simplex
-simplexUniform n = either error id $ simplexFromVector $ V.replicate n (1.0 / fromIntegral n)
+simplexUniform k = either error id $ simplexFromVector $ V.replicate k (1.0 / fromIntegral k)
 
 -- The tuning parameter is the inverted mean of all alpha values.
 dirichletSimple :: Double -> ProposalSimple Simplex
-dirichletSimple invAlphaMean (Simplex xs) g = do
-  let ddXs = either error id $ dirichletDistribution $ V.map (/invAlphaMean) xs
+dirichletSimple t (Simplex xs) g = do
+  -- If @t@ is high and above 1.0, the parameter vector will be low, and the
+  -- variance will be high. If @t@ is low and below 1.0, the parameter vector
+  -- will be high, and the Dirichlet distribution will be very concentrated with
+  -- low variance.
+  let ddXs = either error id $ dirichletDistribution $ V.map (/t) xs
   ys <- dirichletSample ddXs g
-  let ddYs = either error id $ dirichletDistribution $ V.map (/invAlphaMean) ys
+  let ddYs = either error id $ dirichletDistribution $ V.map (/t) ys
       densityXY = dirichletDensity ddXs ys
       densityYX = dirichletDensity ddYs xs
   return (either error id $ simplexFromVector ys, densityYX / densityXY)
 
 -- | Dirichlet proposal.
 --
--- For a given element of a D-dimensional simplex, propose a new element of the
--- D-dimensional simplex. The new element is sampled from the multivariate
+-- For a given element of a K-dimensional simplex, propose a new element of the
+-- K-dimensional simplex. The new element is sampled from the multivariate
 -- Dirichlet distribution with parameter vector being the old element of the
 -- simplex. The tuning parameter is used to determine the concentration of the
 -- Dirichlet distribution: the lower the tuning parameter, the higher the
