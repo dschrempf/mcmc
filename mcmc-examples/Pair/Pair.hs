3322e4b96bca12762dd10049f4a3b72bf7860371
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-05-19 22:56:51 +0200

    fix pair

--- mcmc-examples/Pair/Pair.hs
+++ mcmc-examples/Pair/Pair.hs
@@ -7,34 +7,136 @@
 -- Maintainer  :  dominik.schrempf@gmail.com
 -- Stability   :  unstable
 -- Portability :  portable
+--
+-- This example module is more involved and contains custom proposals with
+-- custom Jacobians. We sample the posterior distribution of a variable \(z \sim
+-- \mbox{Exp}(1.0)\) separated into a sum \(z=x+y\), with \(x,y > 0\). The
+-- sampler propose new values for \(x\) and \(y\).
 module Main
   ( main,
   )
 where
 
-import Control.Lens
 import Control.Monad
 import Mcmc
+import Mcmc.Proposal
+import Statistics.Distribution
+import Statistics.Distribution.Normal
 import System.Random.MWC hiding (uniform)
 
+-- The state is composed of a tuple of two variables x and y.
 type I = (Double, Double)
 
--- Uninformative, improper prior for positive precision values.
+analysisName :: AnalysisName
+analysisName = AnalysisName "Pair"
+
+-- Improper prior for positive values.
 pr :: I -> Log Double
-pr (x, y) = largerThan 0.001 x * largerThan 0.001 y * exponential 1.0 (x * y)
+pr (x, y) = largerThan 0.0001 x * largerThan 0.0001 y
 
 -- Likelihood function.
 lh :: I -> Log Double
-lh _ = 1.0
+lh (x, y) = exponential 1.0 (x + y)
+
+-- Initial value.
+start :: I
+start = (1.1, 1.1)
+
+-- Additive proposals with a custom Jacobian.
+psX :: TuningParameter -> ProposalSimple I
+psX t (x, y) g = do
+  let d = normalDistr 0 t
+  u <- genContinuous d g
+  let x' = x + u
+      qXY = Exp $ logDensity d u
+      qYX = Exp $ logDensity d (negate u)
+      r = abs $ (x + y) / (x + y + u)
+      j = Exp $ log r
+  return ((x', y), qYX / qXY, j)
+
+psY :: TuningParameter -> ProposalSimple I
+psY t (x, y) g = do
+  let d = normalDistr 0 t
+  u <- genContinuous d g
+  let y' = y + u
+      qXY = Exp $ logDensity d u
+      qYX = Exp $ logDensity d (negate u)
+      r = abs $ (x + y) / (x + y + u)
+      j = Exp $ log r
+  return ((x, y'), qYX / qXY, j)
+
+prX :: Proposal I
+prX =
+  Proposal
+    (PName "x")
+    (PDescription "prX")
+    (PDimension 1)
+    (PWeight 1)
+    (psX 1)
+    (Just $ Tuner 1.0 psX)
+
+prY :: Proposal I
+prY =
+  Proposal
+    (PName "y")
+    (PDescription "prY")
+    (PDimension 1)
+    (PWeight 1)
+    (psY 1)
+    (Just $ Tuner 1.0 psY)
+
+-- -- The equivalent multiplicative proposals are commented out.
+
+-- psX :: TuningParameter -> ProposalSimple I
+-- psX t (x, y) g = do
+--   let d = gammaDistr (recip t) t
+--   u <- genContinuous d g
+--   let x' = x * u
+--       qXY = Exp $ logDensity d u
+--       qYX = Exp $ logDensity d (recip u)
+--       r = abs $ recip u * (x + y) / (x' + y)
+--       j = Exp $ log r
+--   return ((x', y), qYX / qXY, j)
+
+-- psY :: TuningParameter -> ProposalSimple I
+-- psY t (x, y) g = do
+--   let d = gammaDistr (recip t) t
+--   u <- genContinuous d g
+--   let y' = y * u
+--       qXY = Exp $ logDensity d u
+--       qYX = Exp $ logDensity d (recip u)
+--       r = abs $ recip u * (x + y) / (x + y')
+--       j = Exp $ log r
+--   return ((x, y'), qYX / qXY, j)
+
+-- prX :: Proposal I
+-- prX =
+--   Proposal
+--     (PName "x")
+--     (PDescription "prX mult")
+--     (PDimension 1)
+--     (PWeight 1)
+--     (psX 1)
+--     (Just $ Tuner 1.0 psX)
+
+-- prY :: Proposal I
+-- prY =
+--   Proposal
+--     (PName "y")
+--     (PDescription "prY mult")
+--     (PDimension 1)
+--     (PWeight 1)
+--     (psY 1)
+--     (Just $ Tuner 1.0 psY)
 
 -- The proposal cycle consists of one proposal only. A uniform distribution is used to
 -- slide the precision of the archer.
 cc :: Cycle I
 cc =
   cycleFromList
-    [ _1 @~ slideSymmetric 1 (PName "x") (PWeight 5) Tune,
-      _2 @~ slideSymmetric 1 (PName "y") (PWeight 5) Tune,
-      scaleContrarily 1.0 1.0 (PName "x y") (PWeight 5) Tune
+    [ prY
+    , prX
+    , slideContrarily 0 1.0 (PName "x y") (PWeight 1) Tune
     ]
 
 -- Monitor the precision of the archer.
@@ -42,7 +144,8 @@ monPs :: [MonitorParameter I]
 monPs =
   [ fst >$< monitorDouble "x",
     snd >$< monitorDouble "y",
-    uncurry (*) >$< monitorDouble "xy"
+    uncurry (+) >$< monitorDouble "x+y",
+    uncurry (*) >$< monitorDouble "x*y"
   ]
 
 -- Monitor to standard output.
@@ -50,7 +153,7 @@ monStd :: MonitorStdOut I
 monStd = monitorStdOut monPs 1000
 
 monFile :: MonitorFile I
-monFile = monitorFile "" monPs 50
+monFile = monitorFile "" monPs 100
 
 -- Combine the monitors.
 mon :: Monitor I
@@ -61,16 +164,16 @@ main = do
   g <- create
   let mcmcS =
         Settings
-          (AnalysisName "test-pair")
-          (BurnInWithAutoTuning 20000 1000)
-          (Iterations 100000)
+          analysisName
+          (BurnInWithAutoTuning 100000 500)
+          (Iterations 500000)
           Overwrite
           Sequential
           NoSave
           LogStdOutAndFile
-          Info
+          Debug
   -- Metropolis-Hastings-Green algorithm.
-  a <- mhg pr lh cc mon TraceAuto (1, 1) g
+  a <- mhg pr lh cc mon TraceAuto start g
   -- -- Metropolic-coupled Markov chain Monte Carlo algorithm.
   -- let mc3S = MC3Settings 3 1
   -- a <- mc3 mc3S pr lh cc mon (1, 1) g
