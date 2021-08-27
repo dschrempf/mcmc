6e524face89fe745ac2b744754401d10e22356c1
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-27 18:05:25 +0200

    use unstable packages for dev

--- flake.nix
+++ flake.nix
@@ -1,55 +1,73 @@
 {
   description = "Development environment for Mcmc";
 
+  inputs.circular.url = "github:dschrempf/circular";
+  inputs.circular.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.dirichlet.url = "github:dschrempf/dirichlet";
+  inputs.dirichlet.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          lib = nixpkgs.lib;
-          packageNames = [
-            "mcmc"
-            "mcmc-examples"
-            "mcmc-statistics"
-          ];
-          mcmc-create-package = f: name: f name (./. + "/${name}") rec {};
-          mcmc-overlay = (
-            selfn: supern: {
-              haskellPackages = supern.haskellPackages.override {
-                overrides = selfh: superh:
-                  lib.genAttrs packageNames
-                    (mcmc-create-package selfh.callCabal2nix);
+  inputs.pava.url = "github:dschrempf/pava";
+  inputs.pava.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    { self
+    , circular
+    , dirichlet
+    , flake-utils
+    , nixpkgs
+    , pava
+    }:
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            lib = nixpkgs.lib;
+            packageNames = [
+              "mcmc"
+              "mcmc-examples"
+              "mcmc-statistics"
+            ];
+            mcmc-create-package = f: name: f name (./. + "/${name}") rec {};
+            mcmc-overlay = (
+              selfn: supern: {
+                haskellPackages = supern.haskellPackages.override {
+                  overrides = selfh: superh:
+                    {
+                      circular = circular.defaultPackage.${system};
+                      dirichlet = dirichlet.defaultPackage.${system};
+                      pava = pava.defaultPackage.${system};
+                    } // lib.genAttrs packageNames
+                      (mcmc-create-package selfh.callCabal2nix);
+                };
+              }
+            );
+            overlays = [ mcmc-overlay ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
+            };
+            # Set with packages.
+            mcmc = lib.genAttrs packageNames (n: pkgs.haskellPackages.${n});
+            # List with packages with benchmark dependencies for development
+            # environment.
+            mcmc-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) mcmc;
+          in
+            {
+              packages = mcmc;
+
+              defaultPackage = mcmc.mcmc;
+
+              devShell = pkgs.haskellPackages.shellFor {
+                packages = _: (builtins.attrValues mcmc-dev);
+                buildInputs = with pkgs.haskellPackages; [
+                  haskell-language-server
+                  cabal-install
+                ];
+                doBenchmark = true;
               };
             }
-          );
-          overlays = [ mcmc-overlay ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-            # TODO: Remove when circular is unbroken.
-            config = { allowBroken = true; };
-          };
-          # Set with packages.
-          mcmc = lib.genAttrs packageNames (n: pkgs.haskellPackages.${n});
-          # List with packages with benchmark dependencies for development
-          # environment.
-          mcmc-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) mcmc;
-        in
-          {
-            packages = mcmc;
-
-            defaultPackage = mcmc.mcmc;
-
-            devShell = pkgs.haskellPackages.shellFor {
-              packages = _: (builtins.attrValues mcmc-dev);
-              buildInputs = with pkgs.haskellPackages; [
-                haskell-language-server
-                cabal-install
-              ];
-              doBenchmark = true;
-            };
-          }
-    );
+      );
 }
