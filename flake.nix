f1c0caf75b7eb046519ffc7fb5434da2a5bed5a9
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-27 09:43:00 +0200

    cabal, flake nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,48 @@
 {
-  description = "Development environment for Mcmc.";
+  description = "Development environment for Mcmc";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
-          pkgs = import nixpkgs { inherit system; };
+          lib = nixpkgs.lib;
+          packageNames = [
+            "mcmc"
+            "mcmc-examples"
+            "mcmc-statistics"
+          ];
+          mcmc-create-package = f: name: f name (./. + "/${name}") rec {};
+          mcmc-overlay = (
+            selfn: supern: {
+              haskellPackages = supern.haskellPackages.override {
+                overrides = selfh: superh:
+                  lib.genAttrs packageNames
+                    (mcmc-create-package selfh.callCabal2nix);
+              };
+            }
+          );
+          overlays = [ mcmc-overlay ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          # Set with packages.
+          mcmc = lib.genAttrs packageNames (n: pkgs.haskellPackages.${n});
+          # List with packages with benchmark dependencies for development
+          # environment.
+          mcmc-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) mcmc;
         in
           {
-            devShell = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [ cabal-install ghc ];
-              buildInputs = with pkgs; [
-                zlib
+            packages = mcmc;
+
+            devShell = pkgs.haskellPackages.shellFor {
+              packages = _: (builtins.attrValues mcmc-dev);
+              buildInputs = with pkgs.haskellPackages; [
+                haskell-language-server
+                cabal-install
               ];
+              doBenchmark = true;
             };
           }
     );
