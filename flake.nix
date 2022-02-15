57efcc9c085202301c7d337dfc50f58256fb49c6
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-02-15 11:31:33 +0100

    rm stack; update cabal

--- flake.nix
+++ flake.nix
@@ -22,58 +22,57 @@
     , nixpkgs
     , pava
     }:
-      flake-utils.lib.eachDefaultSystem (
-        system:
-          let
-            lib = nixpkgs.lib;
-            packageNames = [
-              "mcmc"
-              "mcmc-examples"
-              "mcmc-statistics"
-            ];
-            mcmc-create-package = f: name: f name (./. + "/${name}") rec { };
-            mcmc-overlay = (
-              selfn: supern: {
-                haskellPackages = supern.haskellPackages.override {
-                  overrides = selfh: superh:
-                    {
-                      circular = circular.defaultPackage.${system};
-                      covariance = covariance.defaultPackage.${system};
-                      dirichlet = dirichlet.defaultPackage.${system};
-                      pava = pava.defaultPackage.${system};
-                    } // lib.genAttrs packageNames
-                      (mcmc-create-package selfh.callCabal2nix);
-                };
-              }
-            );
-            overlays = [ mcmc-overlay ];
-            pkgs = import nixpkgs {
-              inherit system overlays;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        lib = nixpkgs.lib;
+        packageNames = [
+          "mcmc"
+          "mcmc-examples"
+          "mcmc-statistics"
+        ];
+        mcmc-create-package = f: name: f name (./. + "/${name}") rec { };
+        mcmc-overlay = (
+          selfn: supern: {
+            haskellPackages = supern.haskellPackages.override {
+              overrides = selfh: superh:
+                {
+                  circular = circular.defaultPackage.${system};
+                  covariance = covariance.defaultPackage.${system};
+                  dirichlet = dirichlet.defaultPackage.${system};
+                  pava = pava.defaultPackage.${system};
+                } // lib.genAttrs packageNames
+                  (mcmc-create-package selfh.callCabal2nix);
             };
-            # When changing the package set, the override above also has to be amended.
-            hpkgs = pkgs.haskellPackages;
-            # Set with packages.
-            mcmc = lib.genAttrs packageNames (n: hpkgs.${n});
-            # List with packages with benchmark dependencies for development
-            # environment.
-            mcmc-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) mcmc;
-          in
-            {
-              packages = mcmc;
+          }
+        );
+        overlays = [ mcmc-overlay ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        # When changing the package set, the override above also has to be amended.
+        hpkgs = pkgs.haskellPackages;
+        # Set with packages.
+        mcmc = lib.genAttrs packageNames (n: hpkgs.${n});
+        # List with packages with benchmark dependencies for development
+        # environment.
+        mcmc-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) mcmc;
+      in
+      {
+        packages = mcmc;
 
-              defaultPackage = mcmc.mcmc;
+        defaultPackage = mcmc.mcmc;
 
-              devShell = hpkgs.shellFor {
-                packages = _: (builtins.attrValues mcmc-dev);
-                buildInputs = with pkgs; [
-                  bashInteractive
-                  hpkgs.cabal-install
-                  hpkgs.haskell-language-server
-                  hpkgs.stack
-                ];
-                doBenchmark = true;
-                withHoogle = true;
-              };
-            }
-      );
+        devShell = hpkgs.shellFor {
+          packages = _: (builtins.attrValues mcmc-dev);
+          buildInputs = with pkgs; [
+            bashInteractive
+            hpkgs.cabal-install
+            hpkgs.haskell-language-server
+          ];
+          doBenchmark = true;
+          withHoogle = true;
+        };
+      }
+    );
 }
