0bf55102cf8ef29b38136dfdd17a1a066b151fc0
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-03-07 14:31:54 +0100

    revert pushA, does not help

--- mcmc/src/Mcmc/Acceptance.hs
+++ mcmc/src/Mcmc/Acceptance.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
-
 -- |
 -- Module      :  Mcmc.Acceptance
 -- Description :  Handle acceptance rates
@@ -50,21 +48,10 @@ instance (Ord k, FromJSONKey k) => FromJSON (Acceptance k) where
 emptyA :: Ord k => [k] -> Acceptance k
 emptyA ks = Acceptance $ M.fromList [(k, (0, 0)) | k <- ks]
 
-incrInt :: Int -> Int
-incrInt x = x'
-  where
-    !x' = x + 1
-{-# INLINE incrInt #-}
-
 -- | For key @k@, prepend an accepted (True) or rejected (False) proposal.
 pushA :: Ord k => k -> Bool -> Acceptance k -> Acceptance k
-pushA k b =
-  Acceptance
-    . M.adjust
-      (\(nA, nR) -> if b then (incrInt nA, nR) else (nA, incrInt nR))
-      k
-    . fromAcceptance
-{-# INLINEABLE pushA #-}
+pushA k True = Acceptance . M.adjust (force . first succ) k . fromAcceptance
+pushA k False = Acceptance . M.adjust (force . second succ) k . fromAcceptance
 
 -- | Reset acceptance storage.
 resetA :: Ord k => Acceptance k -> Acceptance k
