8775dad3731f3bf2f8b90fb612e189ea032ebce4
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-03-23 16:47:56 +0100

    Update GHC

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         mcmc-create-package = f: name: f name (./. + "/${name}") rec { };
         mcmc-overlay = (
           selfn: supern: {
-            haskellPackages = supern.haskell.packages.ghc921.override {
+            haskellPackages = supern.haskell.packages.ghc922.override {
               overrides = selfh: superh:
                 {
                   circular = circular.defaultPackage.${system};
