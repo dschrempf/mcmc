51ce18f9286fe729257c96343255f4d66e88c43d
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-03-23 14:17:14 +0100

    rm unnecesary dontCheck

--- flake.nix
+++ flake.nix
@@ -38,9 +38,6 @@
             haskellPackages = supern.haskell.packages.ghc921.override {
               overrides = selfh: superh:
                 {
-                  # TODO: Doctest fails.
-                  ad = supern.haskell.lib.dontCheck superh.ad;
-
                   circular = circular.defaultPackage.${system};
                   covariance = covariance.defaultPackage.${system};
                   dirichlet = dirichlet.defaultPackage.${system};
