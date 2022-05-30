fd996902ab481a125e4785323ea57a356797b0b6
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-05-30 11:51:04 +0200

    new benchmark results

--- mcmc/bench/Bench.hs
+++ mcmc/bench/Bench.hs
@@ -57,58 +57,69 @@ main = do
     ]
 
 -- benchmarking Normal/Slide
--- time                 42.31 ms   (41.88 ms .. 42.60 ms)
---                      1.000 R²   (0.999 R² .. 1.000 R²)
--- mean                 42.75 ms   (42.52 ms .. 43.29 ms)
--- std dev              661.4 μs   (347.4 μs .. 1.074 ms)
+-- time                 24.12 ms   (23.52 ms .. 24.76 ms)
+--                      0.997 R²   (0.995 R² .. 0.999 R²)
+-- mean                 23.70 ms   (23.44 ms .. 24.02 ms)
+-- std dev              658.2 μs   (448.5 μs .. 837.2 μs)
 
 -- benchmarking Normal/Bactrian
--- time                 45.51 ms   (45.30 ms .. 45.92 ms)
---                      1.000 R²   (0.999 R² .. 1.000 R²)
--- mean                 45.41 ms   (45.31 ms .. 45.61 ms)
--- std dev              276.0 μs   (141.5 μs .. 460.8 μs)
+-- time                 26.65 ms   (26.08 ms .. 27.26 ms)
+--                      0.998 R²   (0.996 R² .. 1.000 R²)
+-- mean                 26.53 ms   (26.32 ms .. 26.91 ms)
+-- std dev              626.9 μs   (322.3 μs .. 1.040 ms)
 
 -- benchmarking Normal/LargeCycle
--- time                 68.82 ms   (67.18 ms .. 70.81 ms)
---                      0.999 R²   (0.997 R² .. 1.000 R²)
--- mean                 67.68 ms   (67.26 ms .. 68.59 ms)
--- std dev              1.074 ms   (618.2 μs .. 1.602 ms)
+-- time                 52.48 ms   (51.51 ms .. 53.83 ms)
+--                      0.999 R²   (0.998 R² .. 1.000 R²)
+-- mean                 52.54 ms   (51.62 ms .. 54.90 ms)
+-- std dev              2.765 ms   (758.0 μs .. 4.788 ms)
+-- variance introduced by outliers: 14% (moderately inflated)
 
 -- benchmarking Poisson
--- time                 72.94 ms   (63.07 ms .. 87.73 ms)
---                      0.953 R²   (0.920 R² .. 1.000 R²)
--- mean                 64.76 ms   (62.84 ms .. 71.62 ms)
--- std dev              5.785 ms   (783.9 μs .. 10.08 ms)
--- variance introduced by outliers: 26% (moderately inflated)
+-- time                 41.84 ms   (40.76 ms .. 42.91 ms)
+--                      0.998 R²   (0.994 R² .. 0.999 R²)
+-- mean                 41.87 ms   (41.39 ms .. 42.59 ms)
+-- std dev              1.201 ms   (782.7 μs .. 1.961 ms)
 
 -- benchmarking MC3/MC3 2
--- time                 13.08 ms   (12.73 ms .. 13.44 ms)
---                      0.993 R²   (0.986 R² .. 0.997 R²)
--- mean                 13.41 ms   (13.16 ms .. 13.72 ms)
--- std dev              682.7 μs   (520.3 μs .. 874.0 μs)
--- variance introduced by outliers: 22% (moderately inflated)
+-- time                 7.771 ms   (7.573 ms .. 8.008 ms)
+--                      0.996 R²   (0.993 R² .. 0.999 R²)
+-- mean                 7.810 ms   (7.722 ms .. 7.974 ms)
+-- std dev              328.3 μs   (223.0 μs .. 517.6 μs)
+-- variance introduced by outliers: 19% (moderately inflated)
 
 -- benchmarking MC3/MC3 3
--- time                 19.19 ms   (18.86 ms .. 19.59 ms)
---                      0.998 R²   (0.996 R² .. 1.000 R²)
--- mean                 19.28 ms   (19.11 ms .. 19.51 ms)
--- std dev              454.1 μs   (339.1 μs .. 608.4 μs)
+-- time                 10.69 ms   (10.57 ms .. 10.87 ms)
+--                      0.997 R²   (0.993 R² .. 1.000 R²)
+-- mean                 10.73 ms   (10.65 ms .. 10.90 ms)
+-- std dev              308.1 μs   (132.2 μs .. 528.9 μs)
 
 -- benchmarking MC3/MC3 4
--- time                 25.01 ms   (24.21 ms .. 25.66 ms)
---                      0.997 R²   (0.996 R² .. 0.999 R²)
--- mean                 24.21 ms   (23.99 ms .. 24.55 ms)
--- std dev              606.1 μs   (414.7 μs .. 738.5 μs)
+-- time                 13.81 ms   (13.66 ms .. 14.08 ms)
+--                      0.999 R²   (0.997 R² .. 1.000 R²)
+-- mean                 13.73 ms   (13.67 ms .. 13.85 ms)
+-- std dev              213.2 μs   (95.38 μs .. 334.4 μs)
 
 -- benchmarking MC3/MC3 5
--- time                 28.39 ms   (26.99 ms .. 29.34 ms)
---                      0.995 R²   (0.990 R² .. 0.999 R²)
--- mean                 31.13 ms   (30.09 ms .. 33.49 ms)
--- std dev              3.009 ms   (984.0 μs .. 4.319 ms)
--- variance introduced by outliers: 40% (moderately inflated)
+-- time                 16.80 ms   (16.65 ms .. 16.93 ms)
+--                      0.999 R²   (0.997 R² .. 1.000 R²)
+-- mean                 16.99 ms   (16.87 ms .. 17.23 ms)
+-- std dev              376.6 μs   (162.3 μs .. 589.3 μs)
 
 -- benchmarking MC3/MC3 10
--- time                 57.25 ms   (56.98 ms .. 57.61 ms)
+-- time                 32.59 ms   (32.10 ms .. 33.20 ms)
+--                      0.999 R²   (0.998 R² .. 1.000 R²)
+-- mean                 32.21 ms   (32.07 ms .. 32.46 ms)
+-- std dev              390.9 μs   (219.6 μs .. 640.0 μs)
+
+-- benchmarking probability/gamma function general
+-- time                 60.73 ms   (60.37 ms .. 61.26 ms)
 --                      1.000 R²   (1.000 R² .. 1.000 R²)
--- mean                 57.46 ms   (57.34 ms .. 57.56 ms)
--- std dev              192.4 μs   (140.1 μs .. 284.0 μs)
+-- mean                 60.70 ms   (60.49 ms .. 60.91 ms)
+-- std dev              385.5 μs   (287.3 μs .. 513.5 μs)
+
+-- benchmarking probability/gamma function specialized
+-- time                 59.00 ms   (58.74 ms .. 59.32 ms)
+--                      1.000 R²   (1.000 R² .. 1.000 R²)
+-- mean                 58.83 ms   (58.76 ms .. 58.97 ms)
+-- std dev              168.3 μs   (105.7 μs .. 245.7 μs)
