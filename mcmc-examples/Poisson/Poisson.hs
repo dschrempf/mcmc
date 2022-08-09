fdd49a847f9b81f5adb120fccf90e82b6fb9b64e
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-08-09 16:01:27 +0200

    comments

--- mcmc-examples/Poisson/Poisson.hs
+++ mcmc-examples/Poisson/Poisson.hs
@@ -42,6 +42,7 @@ ppSc = scaleUnbiased 0.1 (PName "lambda") (pWeight 1) Tune
 cc :: Cycle I
 cc = cycleFromList [ppSl, ppSc]
 
+-- 'monitorDouble' is a simple monitor printing the value of a 'Double'.
 monLambda :: MonitorParameter I
 monLambda = monitorDouble "lambda"
 
@@ -61,14 +62,18 @@ ss =
   Settings
     -- Provide an analysis name.
     (AnalysisName "poisson")
-    -- Burn in for 2000 generations. Auto tune every 200 iterations.
+    -- Burn in for 2000 generations. During burn in, the proposals are tuned
+    -- automatically. This is called "auto tuning". Here, auto tuning is
+    -- performed every 200 iterations.
     (BurnInWithAutoTuning 2000 200)
-    -- Number of iterations after burn in.
+    -- Number of actual iterations after burn in.
     (Iterations 30000)
     -- The trace of the Markov chain contains the attained values. In our case,
     -- it is a vector of fatal accident rates. Here, we tell the sampler to use
-    -- the shortest trace possible. For example, Hamiltonian proposals require
-    -- the trace for auto tuning masses.
+    -- the shortest trace possible. In our case, this will be a single value.
+    -- However, when using batch monitors, or when auto tuning the masses of
+    -- proposals based on Hamiltonian dynamics, the required length of the trace
+    -- is larger than 1. masses. The trace length can also be set manually.
     TraceAuto
     -- Overwrite files created by a possible previous analysis.
     Overwrite
